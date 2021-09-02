b2610a4e4399566dd08465df8312d05ca2c266bc
Morgan Ney <morganney@gmail.com>
2021-09-01 23:01:22 -0500

    Update purge regex

--- src/purge.js
+++ src/purge.js
@@ -1,10 +1,10 @@
 const purge = source => {
   // No Imports: /import\s.+\sfrom\s+'webpack-strip-debug-loader'|.*require\('webpack-strip-debug-loader'\).*/
   // No Declarations: /(var|let|const)?\s+debug(\w?)+\s*=\s*Debug\(.+\)/g
-  // No Invocations: /\s+debug(\w?)+\(('[\w, ]+')?\)/g
+  // No Invocations: /\s*debug(\w?)+\s*?\(.+\)/g
 
   return source.replace(
-    /(import\s.+\sfrom\s+'webpack-strip-debug-loader'|.*require\('webpack-strip-debug-loader'\).*)|((var|let|const)?\s+debug(\w?)+\s*=\s*Debug\(.+\))|(\s+debug(\w?)+\(('[\w, ]+')?\))/g,
+    /(import\s.+\sfrom\s+'webpack-strip-debug-loader'|.*require\('webpack-strip-debug-loader'\).*)|((var|let|const)?\s+debug(\w?)+\s*=\s*Debug\(.+\))|(\s*debug(\w?)+\s*?\(.+\))/g,
     '\n'
   )
 }
