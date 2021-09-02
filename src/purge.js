c1ffa651c157e7ca0f25eafe43bbac35ef39c68c
Morgan Ney <morganney@gmail.com>
2021-09-02 10:45:21 -0500

    Support multiline debug calls with func calls as arguments

--- src/purge.js
+++ src/purge.js
@@ -1,10 +1,10 @@
 const purge = source => {
   // No Imports: /import\s.+\sfrom\s+['"]webpack-strip-debug-loader['"]|.*require\(['"]webpack-strip-debug-loader['"]\).*/
   // No Declarations: /(var|let|const)?\s+debug(\w?)+\s*=\s*Debug\([\s\S]*?.*\)/g
-  // No Invocations: /\s*debug(\w?)+\s*?\([\s\S]*?.*\)/g
+  // No Invocations: /\s*debug(\w?)+\s*?\([\s\S]*?.*\s*\)(?![\),])/g
 
   return source.replace(
-    /(import\s.+\sfrom\s+['"]webpack-strip-debug-loader['"]|.*require\(['"]webpack-strip-debug-loader['"]\).*)|((var|let|const)?\s+debug(\w?)+\s*=\s*Debug\([\s\S]*?.*\))|(\s*debug(\w?)+\s*?\([\s\S]*?.*\))/g,
+    /(import\s.+\sfrom\s+['"]webpack-strip-debug-loader['"]|.*require\(['"]webpack-strip-debug-loader['"]\).*)|((var|let|const)?\s+debug(\w?)+\s*=\s*Debug\([\s\S]*?.*\))|(\s*debug(\w?)+\s*?\([\s\S]*?.*\s*\)(?![\),]))/g,
     '\n'
   )
 }
