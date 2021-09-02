e86752a97598939d6fa526dd127fd34b4e3eb35d
Morgan Ney <morganney@gmail.com>
2021-09-02 09:37:38 -0500

    Update regex

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ You must use the wrapper around [`debug`](https://www.npmjs.com/package/debug) n
 
 Do not:
 * Alias your `import` or `require` of `Debug`
+* Spread your `import` of `require` of `Debug` over more than one line
 * Put more than one `debug()` call on a single line
-* Have a comment including a `debug('call')`
 * Other crazy things
 
 Just make simple debug statements.
@@ -21,15 +21,22 @@ Just make simple debug statements.
 ```js
 import { Debug } from 'webpack-strip-debug-loader'
 
-const debug = Debug('feature')
+// Or use require if you prefer that
+const { Debug } = require("webpack-strip-debug-loader")
+
+const debug = Debug("feature")
 const debugFoo = Debug('foo')
 
 if (somethingOfInterestHappens) {
-  debug('something happened')
+  debug("something happened")
 }
 
 if (foo) {
- debugFoo('foo happened', foo)
+  debugFoo(
+    'foo happened',
+    foo,
+    { ...foo }
+  )
 }
 ```
 
--- src/purge.js
+++ src/purge.js
@@ -1,10 +1,10 @@
 const purge = source => {
-  // No Imports: /import\s.+\sfrom\s+'webpack-strip-debug-loader'|.*require\('webpack-strip-debug-loader'\).*/
-  // No Declarations: /(var|let|const)?\s+debug(\w?)+\s*=\s*Debug\(.+\)/g
-  // No Invocations: /\s*debug(\w?)+\s*?\(.+\)/g
+  // No Imports: /import\s.+\sfrom\s+['"]webpack-strip-debug-loader['"]|.*require\(['"]webpack-strip-debug-loader['"]\).*/
+  // No Declarations: /(var|let|const)?\s+debug(\w?)+\s*=\s*Debug\([\s\S]*?.*\)/g
+  // No Invocations: /\s*debug(\w?)+\s*?\([\s\S]*?.*\)/g
 
   return source.replace(
-    /(import\s.+\sfrom\s+'webpack-strip-debug-loader'|.*require\('webpack-strip-debug-loader'\).*)|((var|let|const)?\s+debug(\w?)+\s*=\s*Debug\(.+\))|(\s*debug(\w?)+\s*?\(.+\))/g,
+    /(import\s.+\sfrom\s+['"]webpack-strip-debug-loader['"]|.*require\(['"]webpack-strip-debug-loader['"]\).*)|((var|let|const)?\s+debug(\w?)+\s*=\s*Debug\([\s\S]*?.*\))|(\s*debug(\w?)+\s*?\([\s\S]*?.*\))/g,
     '\n'
   )
 }
